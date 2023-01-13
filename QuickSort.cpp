b927e2dc02e2b0494492f4670838d12081de5f9a
verilye <connor.j.logan@outlook.com>
2023-01-13 21:01:38 +1000

    more quicksort comments

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 using namespace std;
+
+
+// The quicksort finds the correct postion of 1 value
+// by putting all lower values to the left, iterating once
+// then swapping to that location. Repeat until all of these
+// incorrect 'partitions' are sorted
+
+
+//Swap two elements 
   
-// A utility function to swap two elements
 void swap(int* a, int* b)
 {
     int t = *a;
@@ -10,45 +18,47 @@ void swap(int* a, int* b)
     *b = t;
 }
   
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
+
+// 
+
 int partition(int arr[], int low, int high)
 {
     int pivot = arr[high]; // pivot
-    int i
-        = (low
-           - 1); // Index of smaller element and indicates
-                 // the right position of pivot found so far
+    int counter = (low - 1); //keep track of all small values sent to the front
+
+
+    // iterate through values and swap all values lower than pivot to the front
   
     for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than the pivot
         if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
+            counter++;
+            swap(&arr[counter], &arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+
+    // moving the value to this position guarantees that it is in the correct postion
+    // in the array, then we can quicksort every value before and after it
+
+    swap(&arr[counter + 1], &arr[high]);
+    return (counter + 1);
 }
-  
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
+
+
+
+// A single quicksort finds the correct position of the pivot in the array
+// by putting all smaller values before it
+// Once we find the correct location of a single value, it creates 'partitions'
+// that are incorrect around the correct value, breaking down the values that 
+// need to be sorted into smaller groups
+
 void quickSort(int arr[], int low, int high)
 {
     if (low < high) {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        int pi = partition(arr, low, high);
-  
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+
+        int correctIndex = partition(arr, low, high);
+
+        quickSort(arr, low, correctIndex - 1);
+        quickSort(arr, correctIndex + 1, high);
     }
 }
   
